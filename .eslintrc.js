925144bac0cdf9a34150dbfd88942f8760de720c
shenqistart <kevinwangyue@126.com>
2019-01-10 07:54:50 +0800

    样式准备

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// 方法的花括号距离space-before-function-paren，缩进值indent
 module.exports = {
   root: true,
   env: {
@@ -9,7 +10,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,
+    'indent': 0,
   },
   parserOptions: {
     parser: 'babel-eslint'
