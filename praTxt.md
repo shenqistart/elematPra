a347e6f3075c3694629474e93fb1ab049d600dc9
shenqistart <kevinwangyue@126.com>
2019-01-13 23:28:21 +0800

    基础tab

--- praTxt.md
+++ praTxt.md
@@ -1,3 +1,40 @@
+2.3api接口
+data.json
+webpack中的devserver的字段
+webpack本身就是基于express写的
+
+3.1
 v-header
 注意props要有返回值否则会在引用的时候报错
 深度的比如数组这些也要注意用v-if判断一下使用
+
+
+3.2数据获取
+const ERR_OK=0;
+柯理化的封装api请求接口,将url放到函数的内层
+promise的书写
+
+3.3用户蒙层
+star模块的制作，数组的思想
+
+3.4header-detail的交互
+全局的fix最好在body下创建
+这种类似插件的使用的组件功能
+
+4.1tab
+.tab {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  >>>.cube-tab {
+    padding: 10px 0;
+  }
+
+  .slide-wrapper {
+    flex: 1;
+    overflow: hidden;
+  }
+}
+https://vue-loader.vuejs.org/zh/guide/scoped-css.html#%E5%AD%90%E7%BB%84%E4%BB%B6%E7%9A%84%E6%A0%B9%E5%85%83%E7%B4%A0
+在scoped中修改引入组件的样式
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
-    <header-detail></header-detail>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import VHeader from "components/v-header/v-header";
-import HeaderDetail from "components/header-detail/header-detail";
 import { getSeller } from "api";
-
+import Tab from "components/tab/tab";
+import Goods from "components/goods/goods";
+import Ratings from "components/ratings/ratings";
+import Seller from "components/seller/seller";
 export default {
   name: "app",
   data() {
@@ -24,14 +28,50 @@ export default {
       });
     }
   },
+  computed: {
+    tabs() {
+      return [
+        {
+          label: "商品",
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: "评论",
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: "商家",
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ];
+    }
+  },
   created() {
     this._getSeller();
   },
   components: {
     VHeader,
-    HeaderDetail
+    Tab
   }
 };
 </script>
-<style lang="stylus">
+<style lang="stylus" scoped>
+#app {
+  .tab-wrapper {
+    position: fixed;
+    top: 136px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+}
 </style>
